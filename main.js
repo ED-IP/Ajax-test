c76d64799d0001fd388cd5fc2026b64bea775c2b
ED-IP <edip_courses@outlook.es>
2020-07-09 08:05:44 +0000

    timeout test

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 var xhr = new XMLHttpRequest();
 
+
 xhr.open("GET" , "https://ci-swapi.herokuapp.com/api/");
 xhr.send();
 
+
 xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200){
-        document.getElementById("data").innerHTML = this.responseText;
+        /*document.getElementById("data").innerHTML = this.responseText;*/
+        data = JSON.parse(this.responseText);
     }
 };
+
+
+setTimeout(function() {
+    console.log(data);
+}, 500);
+
+
