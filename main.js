4483e212aca8453feca5947ca74b1ea4be8b0608
ED-IP <edip_courses@outlook.es>
2020-07-09 08:13:52 +0000

    test callbacks

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
-var xhr = new XMLHttpRequest();
+function getData(cb) {
+    var xhr = new XMLHttpRequest();
 
+    xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+    xhr.send();
 
-xhr.open("GET" , "https://ci-swapi.herokuapp.com/api/");
-xhr.send();
-
-
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200){
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
         /*document.getElementById("data").innerHTML = this.responseText;*/
-        data = JSON.parse(this.responseText);
-    }
-};
+        cb(JSON.parse(this.responseText));
+        }
+    };
+}
 
-
-setTimeout(function() {
+function printDataToConsole(data) {
     console.log(data);
-}, 500);
+}
 
-
+getData (printDataToConsole);
